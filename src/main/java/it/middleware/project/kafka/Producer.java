3db92b622094d82e9679c4c6b09fb664d8ea8ef5
Furcanzo <roberto.buratti@mail.polimi.it>
2021-02-08 12:27:25 +0100

    implemented producer side

--- src/main/java/it/middleware/project/kafka/Producer.java
+++ src/main/java/it/middleware/project/kafka/Producer.java
@@ -1,54 +1,71 @@
 package it.middleware.project.kafka;
 
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class Producer {
-    private static final int waitBetweenMsgs = 500;
+
     private static final boolean waitAck = true;
 
     private static final String serverAddr = "localhost:9092";
 
+    @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
-        // If there are no arguments, publish to the default topic
-        // Otherwise publish on the topics provided as argument
-        String topic = args[0];
 
-        final Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverAddr);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
-        // Idempotence = exactly once semantics between the producer and the partition
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, String.valueOf(true));
+        Gson gson = new Gson();
 
-        final KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
-        final String value = args[1];
-        System.out.println(
-                "Topic: " + topic +
-                        "\tValue: " + value
-        );
-
-        final ProducerRecord<String, String> record = new ProducerRecord<>(topic, value);
-        final Future<RecordMetadata> future = producer.send(record);
-
-        if (waitAck) {
+        while (true) {
             try {
-                RecordMetadata ack = future.get();
-                System.out.println("Ack for topic " + ack.topic() + ", partition " + ack.partition() + ", offset " + ack.offset());
-            } catch (InterruptedException | ExecutionException e1) {
-                e1.printStackTrace();
+                String line = stdin.readLine();
+                JsonObject obj = gson.fromJson(line, JsonObject.class);
+                String topic = obj.get("topic").toString().replace("\"", "");
+                String payload = obj.get("payload").toString();
+
+                final Properties props = new Properties();
+                props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverAddr);
+                props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+                props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+                // Idempotence = exactly once semantics between the producer and the partition
+                props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, String.valueOf(true));
+
+                final KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+                System.out.println(
+                        "Topic: " + topic +
+                                "\tValue: " + payload
+                );
+
+                final ProducerRecord<String, String> record = new ProducerRecord<>(topic, payload);
+                final Future<RecordMetadata> future = producer.send(record);
+
+                if (waitAck) {
+                    try {
+                        RecordMetadata ack = future.get();
+                        System.out.println("Ack for topic " + ack.topic() + ", partition " + ack.partition() + ", offset " + ack.offset());
+                    } catch (InterruptedException | ExecutionException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+                producer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
-        producer.close();
     }
 }
